3e52ad1952c8ac5b769e7fe2c15f35843967b7a2
Kada Ozaki <kada02ak1@outlook.com>
2026-05-20 22:51:19 +0200

    use home manager activation

--- nix/modules/home-manager.nix
+++ nix/modules/home-manager.nix
@@ -1,11 +1,9 @@
-inputs:
-{
+inputs: {
   config,
   lib,
   pkgs,
   ...
-}:
-let
+}: let
   inherit (pkgs.stdenv.hostPlatform) system;
   package = inputs.self.packages.${system}.default;
   configDir =
@@ -13,25 +11,20 @@ let
     + (lib.optionalString pkgs.stdenv.hostPlatform.isDarwin "Profiles/");
 
   cfg = config.textfox;
-in
-{
-
+in {
   imports = [
     ./options.nix
-    (lib.mkChangedOptionModule [ "textfox" "profile" ] [ "textfox" "profiles" ] (
-      config:
-      let
-        profile = lib.getAttrFromPath [ "textfox" "profile" ] config;
-
-      in
-      [ profile ]
+    (lib.mkChangedOptionModule ["textfox" "profile"] ["textfox" "profiles"] (
+      config: let
+        profile = lib.getAttrFromPath ["textfox" "profile"] config;
+      in [profile]
     ))
   ];
 
   options.textfox = {
     profiles = lib.mkOption {
       type = with lib.types; listOf str;
-      default = [ ];
+      default = [];
       description = "List of Firefox profiles to apply the textfox configuration to";
     };
   };
@@ -44,25 +37,42 @@ in
           "${profile}" = {
             extraConfig = builtins.readFile "${package}/user.js";
             containersForce = true;
-            userChrome = lib.mkBefore (builtins.readFile "${package}/chrome/userChrome.css");
+            # userChrome = lib.mkBefore (builtins.readFile "${package}/chrome/userChrome.css");
           };
-        }) cfg.profiles
+        })
+        cfg.profiles
       );
     };
 
-    home.file = lib.mkMerge (
-      map (profile: {
-        "${configDir}${profile}/chrome" = {
-          source = pkgs.lib.cleanSourceWith {
-            src = "${package}/chrome";
-            filter = path: type: !(type == "regular" && baseNameOf path == "userChrome.css");
-          };
-          recursive = true;
-        };
-        "${configDir}${profile}/chrome/config.css" = {
-          text = cfg.configCss;
-        };
-      }) cfg.profiles
-    );
+    home.activation = let
+      configCss = pkgs.writeText "config.css" cfg.configCss;
+    in
+      lib.listToAttrs (
+        map (
+          profile: {
+            name = "copyTextfox${profile}";
+            value =
+              lib.hm.dag.entryAfter
+              ["linkGeneration"]
+              ''
+                cd "${package}"
+                SRC_FILES=$(find . -type f | grep ./chrome)
+                PROFILE_DIR="${configDir}${profile}"
+                cd "$HOME/$PROFILE_DIR"
+                for file in $SRC_FILES; do
+                  dirname=$(dirname "$file")
+                  if [ ! -d "$dirname" ]; then
+                    mkdir -p "$dirname"
+                  fi
+                  cp -L "${package}/$file" "$HOME/$PROFILE_DIR/$file"
+                  chmod 744 "$HOME/$PROFILE_DIR/$file"
+                done
+                cp -L ${configCss} "$HOME/$PROFILE_DIR/chrome/config.css"
+                chmod 744 "$HOME/$PROFILE_DIR/chrome/config.css"
+              '';
+          }
+        )
+        cfg.profiles
+      );
   };
 }
